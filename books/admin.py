f1f52dfecf5d142cf2fed486242d549e37378dee
Hans-Jörg Schurr <hans-joerg@schurr.at>
2016-02-12 23:37:34 +0100

    More accurat models

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Book, Bookshelf
+from django.forms import TextInput
+from django.db import models
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
+
 admin.site.register(Bookshelf)
 
 # Register your models here.
--- books/models.py
+++ books/models.py
@@ -2,39 +2,43 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+# fields starting with bc correspond to a book catalogue field
 class Book(models.Model):
     user = models.ForeignKey(User)
-    author_details = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=13)
-    publisher = models.CharField(max_length=200)
-    date_published = models.DateField(null=True, blank=True)
-    rating = models.PositiveSmallIntegerField() # rating from 0-5
-    bookshelfs = models.ManyToManyField("Bookshelf", blank=True)
-    read = models.BooleanField()
-    series_details = models.CharField(max_length=200)
-    pages = models.PositiveIntegerField(null=True, blank=True)
-    notes = models.TextField()
-    list_price = models.PositiveIntegerField(null=True, blank=True)
+    bc_author_details = models.TextField("Author Details")
+    bc_title = models.TextField("Title")
+    bc_isbn = models.TextField("ISBN",null=True, blank=True)
+    bc_publisher = models.TextField("Publisher",null=True, blank=True)
+    bc_date_published = models.DateField("Date Published",null=True, blank=True)
+    bc_rating = models.PositiveSmallIntegerField("Rating", default=0) # rating from 0-5
+    bc_bookshelfs = models.ManyToManyField("Bookshelf", blank=True)
+    bc_read = models.BooleanField("Read")
+    bc_series_details = models.TextField("Series Details",null=True, blank=True)
+    bc_pages = models.PositiveIntegerField("Pages",null=True, blank=True)
+    bc_notes = models.TextField("Notes",null=True, blank=True)
+    bc_list_price = models.TextField("List Price",null=True, blank=True)
     ANTHOLOGY_CHOICES = [
-            ('0',"No."),
-            ('1',"Yes, all by the same author."),
-            ('2',"Yes, different authors.")
+            (0,"No."),
+            (1,"Yes, all by the same author."),
+            (2,"Yes, different authors.")
     ]
-    anthology = models.BooleanField()
-    location = models.CharField(max_length=200)
-    read_start = models.DateField(null=True, blank=True)
-    read_end = models.DateField(null=True, blank=True)
-    book_format = models.CharField(max_length=200)
-    signed = models.BooleanField()
-    loaned_to = models.CharField(max_length=200)
-    anthology_titles = models.CharField(max_length=400)
-    description = models.TextField()
-    genre = models.CharField(max_length=200)
-    date_added = models.DateTimeField()
-    last_update_date = models.DateTimeField()
-    uuid = models.UUIDField(default=uuid.uuid4)
+    bc_anthology = models.IntegerField("Anthology",default=0, choices=ANTHOLOGY_CHOICES)
+    bc_location = models.TextField("Location", null=True, blank=True)
+    bc_read_start = models.DateField("Read Start",null=True, blank=True)
+    bc_read_end = models.DateField("Read End",null=True, blank=True)
+    bc_book_format = models.TextField("Book Format", null=True, blank=True)
+    bc_signed = models.BooleanField("Signed",default = False)
+    bc_loaned_to = models.TextField("Loaned To", null=True, blank=True)
+    bc_anthology_titles = models.TextField("Anthology Titles", null=True, blank=True)
+    bc_description = models.TextField("Description", null=True, blank=True)
+    bc_genre = models.TextField("Genre", null=True, blank=True)
+    bc_language = models.TextField("Language", null=True, blank=True )
+    bc_date_added = models.DateTimeField("Date Added")
+    bc_goodreads_bookid = models.IntegerField("Goodreads Book ID", null=True, blank=True)
+    bc_goodreads_sync_date = models.DateField("Goodreads Sync Date",null = True, blank=True)
+    bc_last_update_date = models.DateTimeField("Update Date")
+    bc_uuid = models.UUIDField("UUID",default=uuid.uuid4)
 
 class Bookshelf(models.Model):
-    export_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=200)
+    bc_export_id = models.PositiveIntegerField("Export ID")
+    bc_name = models.TextField("Name",null=True, blank=True)
